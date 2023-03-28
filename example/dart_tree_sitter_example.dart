222ba194364b66e108234e75b3a12f15a994a0ee
NTBBloodbath <bloodbathalchemist@protonmail.com>
2023-03-28 04:24:39 -0400

    Initial commit (already bundles `tree-sitter`)

---
+++ example/dart_tree_sitter_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_tree_sitter/dart_tree_sitter.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dart_tree_sitter.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_tree_sitter;
+
+export 'src/dart_tree_sitter_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
